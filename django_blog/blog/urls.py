40ad665a5929abb8c236118dbe2a00600b25b08c
Abdallah <abdallahmohamedama12@gmail.com>
2025-10-19 02:50:46 +0300

    feat: add profile page template and update URL routing for user profiles

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
+from django.contrib.auth import views as auth_views
+from django.urls import path
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),  # 👈 added profile URL
 ]

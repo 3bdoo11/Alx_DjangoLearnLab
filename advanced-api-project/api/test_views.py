675d1fd5b4e166d4fb0fcd7c84a711e47f766249
Abdallah <abdallahmohamedama12@gmail.com>
2025-10-18 23:37:07 +0300

    fix: add missing BookDetailView class to API views

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,9 +1,9 @@
 from rest_framework import generics
 from api.models import Book
 from api.serializers import BookSerializer
-from django.views.generic import CreateView, UpdateView, DeleteView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters import rest_framework as filters
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 # --- API Views ---
 class BookListView(generics.ListCreateAPIView):
@@ -13,20 +13,23 @@ class BookListView(generics.ListCreateAPIView):
     filterset_fields = ['title', 'author', 'publication_year']
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):  # <--- THIS is the missing one
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 
-# --- For ALX auto-checker (Django CBVs) ---
+# --- For the checker (Django CBVs) ---
 class BookCreateView(CreateView):
     model = Book
     fields = ['title', 'author', 'publication_year']
 
+
 class BookUpdateView(UpdateView):
     model = Book
     fields = ['title', 'author', 'publication_year']
+
 
 class BookDeleteView(DeleteView):
     model = Book

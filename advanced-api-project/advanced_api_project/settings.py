6b9248011179f33e0ae251d5ea9562cd602dd420
Abdallah <abdallahmohamedama12@gmail.com>
2025-10-18 23:02:44 +0300

    feat: add django_filters to installed apps and update cache files for API routing and views

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',     
-    'api',                 
+    'api',
+    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

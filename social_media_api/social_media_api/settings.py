76d9301a8db9412d339abf83df9d4791a43382d2
Abdallah <abdallahmohamedama12@gmail.com>
2025-10-20 00:54:11 +0300

    fix: remove extra commas in INSTALLED_APPS for syntax correctness

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -36,13 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',,,
+    'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
     'accounts',
-
-
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

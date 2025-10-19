8195f8b863175b0b26c94f6c3dced4ad13d1d1ac
Abdallah <abdallahmohamedama12@gmail.com>
2025-10-19 04:40:18 +0300

    fix: update comment creation view to use consistent URL parameter

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -77,14 +77,13 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     template_name = 'blog/comment_form.html'
 
     def form_valid(self, form):
-        post_pk = self.kwargs['post_pk']
-        post = get_object_or_404(Post, pk=post_pk)
+        post = get_object_or_404(Post, pk=self.kwargs['pk'])
         form.instance.author = self.request.user
         form.instance.post = post
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse_lazy('post_detail', kwargs={'pk': self.kwargs['post_pk']})
+        return reverse_lazy('post_detail', kwargs={'pk': self.kwargs['pk']})
 
 
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
